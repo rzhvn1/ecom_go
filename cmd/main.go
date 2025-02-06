021f29df9589c68e730c4b277504c6c1a4cb1f31
Erzhan <erzhanmuratov99@gmail.com>
2025-02-06 18:09:00 +0600

    feat: setup server and database

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"ecom_go/cmd/api"
+	"ecom_go/configs"
+	"ecom_go/db"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello ecom!")
+	cfg := mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	db, err := db.NewMySQLStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(fmt.Sprintf(":%s", configs.Envs.Port), db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+	
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
